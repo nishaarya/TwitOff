aa0afc4aadf217602b1183c5c24c77cd5558234b
nishaarya <nishaaryaahmed@gmail.com>
2019-12-18 13:07:15 +0000

    Added twitter.py file to connect to twitter API

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,8 +1,9 @@
 """Main application for Twitoff"""
 
 #imports
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 #create app factory
 #creates and configure an instance of a flask app
@@ -10,10 +11,16 @@ def create_app():
     app = Flask(__name__)
 
     #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "Welcome to our app"
+        users = User.query.all()
+        return render_template('base.html', title = 'Home', users=users)
     return app
