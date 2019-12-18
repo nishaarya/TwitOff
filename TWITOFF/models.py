c155138292ef2d38c75c76a6aaf58a2a0e0c295c
nishaarya <nishaaryaahmed@gmail.com>
2019-12-18 10:03:53 +0000

    changing schema formatting

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -10,11 +10,16 @@ DB = SQLAlchemy()
 class User(DB.Model):
     id  = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User{}>'.format(self.name)
 
 #Tweets
 class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
-    name = DB.Column(DB.Unicode(300))
+    text = DB.Column(DB.Unicode(300))
     #the below 2 rows are going to allow us to connect user.id to tweets
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets'), lazy=True)
+    def __repr__(self):
+        return '<Tweet{}>'.format(self.text)
